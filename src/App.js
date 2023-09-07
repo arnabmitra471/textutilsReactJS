775cde2ca0e6d1a7422a5ab34d6792f9d26936d8
Arnab <arnabmitra471@gmail.com>
2023-09-07 21:38:48 +0530

    Created About.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Navbar title="TextUtils" aboutText="About Us"/>
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze below"/>
+        {/* <About/> */}
       </div>
     </>
   );
