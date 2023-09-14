d62785e703e2d4d583a5c1fc3fc5b637393b5da4
Arnab <arnabmitra471@gmail.com>
2023-09-14 19:40:45 +0530

    Added the alert component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 import React,{useState} from 'react';
 function App() {
   const [mode,setMode] = useState("light"); // tells whether dark mode is enabled or not
@@ -20,6 +21,7 @@ function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About Us" mode={mode} toggleMode = {toggleMode}/>
+      <Alert/>
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze below" mode={mode} toggleMode={toggleMode}/>
         {/* <About/> */}
